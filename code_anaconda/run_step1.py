b414f9015509d73f36a2aecbb7f8241f1b25605a
Max Joseph <maxwellbjoseph@gmail.com>
2018-11-28 02:05:04 +0000

    add point groupings and data extraction steps

--- code_anaconda/run_step1.py
+++ code_anaconda/run_step1.py
@@ -1,14 +1,12 @@
 # based on
 # https://stackoverflow.com/questions/4992400/running-several-system-commands-in-parallel
-
+import subprocess
 from subprocess import Popen, PIPE
 import datetime
 import os
+import shlex
 
 def main(tag, dt0, dt1, vorimp='scipy', gt=3, buf0=False, ver=None):
-
-#    yr=2016
-
     if ver is None:
         
         if vorimp == 'scipy':
@@ -52,70 +50,20 @@ def main(tag, dt0, dt1, vorimp='scipy', gt=3, buf0=False, ver=None):
         else:
             raise RuntimeError('Unknown vorimp: %s' % vorimp)
 
-
-    #max_procs = 6 # ask Garth or somebody how many process would be approprite...
     max_procs = 2
 
     # run the prep script
-    if True:
+    print("starting prep: %s" % datetime.datetime.now())
+    cmd = " ".join(['psql'] + ['-f', ('step1_prep_%s.sql' % ver)] + ['-v', ("tag=%s" % tag)])
+    subprocess.run(shlex.split(cmd))
 
 
-        print("starting prep: %s" % datetime.datetime.now())
-        fo = open('out.step1.o0', 'w')
-        p = Popen( 
-            ['psql'] + 
-            ['-f', ('step1_prep_%s.sql' % ver)] +
-            ['-v', ("tag=%s" % tag)],
-            stdout = fo)
+    dates = [dt0 + datetime.timedelta(days=n) for n in
+            range((dt1-dt0).days)]
 
-        p.communicate()
-        if p.returncode >0:
-            print(p)
-            print(dir(p))
-            print(p.stderr)
-            raise RuntimeError()
-
-    if True:
-
-        dates = [dt0 + datetime.timedelta(days=n) for n in
-                range((dt1-dt0).days)]
-
-        procs = set()
-        for dt in dates:
-
-            print("starting work %s: %s" % (dt.strftime('%Y-%m-%d'), datetime.datetime.now()))
-            
-
-#            procs.add(Popen(
-#                ['psql',] +
-#                ['-f', ('step1_work_%s.sql' % ver)] +
-#                ['-v', ("tag=%s" % tag)] +
-#                ['-v', ("oned='%s'" % dt.strftime('%Y-%m-%d'))],
-#                    stdout = open('out.step1.o%s' % dt.strftime('%Y%m%d'),
-#                        'w')
-#                    )) 
-#            if len(procs) > max_procs:
-#                os.wait()
-#                procs.difference_update(
-#                        [p for p in procs if p.poll() is not None]
-#                        )
-#            for p in procs:
-#                if p.poll() is None:
-#                    p.wait()
-#
-            # alright, just one at a time...
-            p = Popen(
-                ['psql',] +
-                ['-f', ('step1_work_%s.sql' % ver)] +
-                ['-v', ("tag=%s" % tag)] +
-                ['-v', ("oned='%s'" % dt.strftime('%Y-%m-%d'))],
-                    stdout = open('out.step1.o%s' % dt.strftime('%Y%m%d'),
-                        'w')
-                    ) 
-            p.communicate()
-            if p.returncode >0:
-                raise RuntimeError()
-
-
-
-
+    procs = set()
+    for dt in dates:
+        print("starting work %s: %s" % (dt.strftime('%Y-%m-%d'), datetime.datetime.now()))
+        cmd = " ".join(['psql',] + ['-f', ('step1_work_%s.sql' % ver)] + 
+                       ['-v', ("tag=%s" % tag)] + ['-v', ("oned='%s'" % dt.strftime('%Y-%m-%d'))])
+        subprocess.run(shlex.split(cmd))
