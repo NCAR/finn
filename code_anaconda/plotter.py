b3d2a3208c0c40e7c9090cd5fbd073b233da061c
Yosuke Kimura <yosukefk@users.noreply.github.com>
2019-04-13 15:38:58 -0500

    now look decent

--- code_anaconda/plotter.py
+++ code_anaconda/plotter.py
@@ -44,20 +44,6 @@ def getinfo(ds):
 
     return o
 
-def point_density(schema_table, conn = None):
-    conn = psycopg2.connect(dbname=os.environ['PGDATABASE'])
-    cur = conn.cursor()
-
-    # determine bbox
-    cur.execute("""
-    SELECT ST_Extent(geom) from %s;""" % schema_table)
-    x = cur.fetchone()[0]
-    print(x)
-
-    # determine raser structure
-    # count density (do thie in database as scratch table)
-    pass
-
 class Plotter(object):
 
     def __init__(self):
@@ -68,56 +54,84 @@ class Plotter(object):
 
     def mk_density(self, schema_table):
 
-        # split schema name out...
-        x = schema_table.split('.')
-        assert len(x) == 2
-        schema_name, table_name = x
+        # TODO break extent and counting into function, and apply to series of schema_table
 
-        # determine bbox
-        self.cur.execute("""
-                WITH foo AS (
-                SELECT ST_Extent(geom) bbox FROM %s
+        def get_extent(st):
+            # determine bbox
+            self.cur.execute("""
+                    WITH foo AS (
+                    SELECT ST_Extent(geom) bbox FROM %s
+                    )
+                    SELECT ST_XMin(foo.bbox), ST_YMin(foo.bbox), ST_XMax(foo.bbox), ST_YMax(foo.bbox)
+                    FROM foo
+                    ;""" % st)
+            x = np.array(self.cur.fetchone())
+            x[:2] = np.floor(x[:2])
+            x[2:] = np.ceil(x[2:])
+            return x
+
+        def count_density(st, ext, dx, arr = None):
+            # count density (do thie in database as scratch table)
+            qry = """ 
+            WITH foo as (
+                SELECT 
+                floor( ( ( ST_X(geom) ) - ( %(x0)f ) ) / %(dx)f ) idx,
+                floor( ( ( %(y1)f ) - ( ST_Y(geom) ) ) / %(dx)f ) jdx
+                FROM %(st)s
+            )
+            SELECT idx, jdx, count(*)
+            FROM foo
+            GROUP BY idx, jdx
+            ;""" % dict(
+                x0 = ext[0],
+                y1 = ext[3],
+                dx = dx,
+                st = st,
                 )
-                SELECT ST_XMin(foo.bbox), ST_YMin(foo.bbox), ST_XMax(foo.bbox), ST_YMax(foo.bbox)
-                FROM foo
-                ;""" % schema_table)
-        x = np.array(self.cur.fetchone())
-        x[:2] = np.floor(x[:2])
-        x[2:] = np.ceil(x[2:])
-        print('x:', x)
+            self.cur.execute(qry)
+            cnts = np.array(self.cur.fetchall())
+
+            if arr is None:
+                arr = np.zeros(n[::-1])
+            arr[cnts[:,1].astype(int), cnts[:,0].astype(int)] = cnts[:,2]
+
+            return arr
+
+        # the code works for list of schema/table combo.
+        # if single value is passed, called itself passing the argument as one element list
+
+        if isinstance(schema_table, str):
+            return self.mk_density([ schema_table,])
+
+
+        # get extent of each table, then find a large enough extent covering all
+        print(schema_table)
+        exts = [get_extent(st) for st in schema_table]
+        exts = np.vstack(exts)
+        print(exts)
+
+        ext = np.hstack([np.amin(exts[:, :2], axis=0), np.amax(exts[:,2:], axis=0)])
+
 
         # determine raser structure
         # i am using 6 sec raster, and 32 256 overview
         # for now assume that o_32 resultion fits all
         dx = 32*6/60/60
-        d = x[2:] - x[:2]
+        d = ext[2:] - ext[:2]
         n = np.ceil(d * (60 * 60 / (6 * 32))).astype(int)
 
 
-        # count density (do thie in database as scratch table)
-        qry = """ 
-        WITH foo as (
-            SELECT 
-            floor( ( ( ST_X(geom) ) - ( %(x0)f ) ) / %(dx)f ) idx,
-            floor( ( ( %(y1)f ) - ( ST_Y(geom) ) ) / %(dx)f ) jdx
-            FROM %(st)s
-        )
-        SELECT idx, jdx, count(*)
-        FROM foo
-        GROUP BY idx, jdx
-        ;""" % dict(
-            x0 = x[0],
-            y1 = x[3],
-            dx = dx,
-            sn = schema_name,
-            st = schema_table,
-            )
-        self.cur.execute(qry)
-        cnts = np.array(self.cur.fetchall())
+        # now count the record using the raster structure
+        arr = None
+        for st in schema_table:
+            arr = count_density( st, ext, dx, arr)
 
-        arr = np.zeros(n[::-1])
-        arr[cnts[:,1].astype(int), cnts[:,0].astype(int)] = cnts[:,2]
+        #arr = np.log10(arr)
+        arr = np.sqrt(arr)
 
+
+
+        # convert the array of counts into geotiff format
         vsipath = '/vsimem/from_postgis'
 
         drv = gdal.GetDriverByName('GTiff')
@@ -125,7 +139,7 @@ class Plotter(object):
         ds = drv.Create(vsipath, xsize = arr.shape[1], ysize = arr.shape[0],
                 bands = 1    , eType = gdal.GDT_Float32)
 
-        ds.SetGeoTransform([x[0], dx, 0, x[3], 0, -dx ])
+        ds.SetGeoTransform([ext[0], dx, 0, ext[3], 0, -dx ])
         srs = osr.SpatialReference()
         srs.ImportFromEPSG(4326)
         ds.SetProjection(srs.ExportToWkt())
@@ -134,9 +148,9 @@ class Plotter(object):
         b.SetNoDataValue(0)
         b.WriteArray(arr)
 
-        dsx = drv.CreateCopy('test2.tif', ds, strict=0)
-        dsx = None
-        del dsx
+        #dsx = drv.CreateCopy('test2.tif', ds, strict=0)
+        #dsx = None
+        #del dsx
 
 
 
@@ -177,6 +191,8 @@ class Plotter(object):
             arr = read_one_band(b)
             if isinstance(cmap, str):
                 cmap = clr_to_cmap(cmap, arr)
+            elif density:
+                cmap = plt.get_cmap('YlOrRd')
 
         elif nb == 3:
             arr = [read_one_band(ds.GetRasterBand(_+1)) for _ in range(3)]
@@ -225,79 +241,10 @@ plotter = Plotter()
 
 
 
-def plot(schema_table, cmap=None, point=False):
-    conn = psycopg2.connect(dbname=os.environ['PGDATABASE'])
-    cur = conn.cursor()
+def plot(schema_table, cmap=None, point=False, **kwds):
+    # this is obsolete, moved in to Plotter's method
+    plotter.plot(schema_table,  **kwds)
 
-
-    cur.execute("""
-    SELECT ST_AsGDALRaster(ST_Union(rast), 'GTiff') FROM %s ;""" % (schema_table))
-
-    vsipath = '/vsimem/from_postgis'
-
-    gdal.FileFromMemBuffer(vsipath, bytes(cur.fetchone()[0]))
-
-    ds = gdal.Open(vsipath)
-    for k, v in getinfo(ds).items():
-        print(k, ":", v)
-    nb = ds.RasterCount
-    for i in range(nb):
-        b = ds.GetRasterBand(i+1)
-        print(band_info(b))
-
-    
-    def read_one_band(b):
-        arr = b.ReadAsArray()
-        # need to flup upside down
-        arr = arr[::-1]
-        arr = np.ma.masked_values(arr, b.GetNoDataValue())
-        return arr
-
-    if nb == 1:
-        b = ds.GetRasterBand(1)
-        arr = read_one_band(b)
-        if isinstance(cmap, str):
-            cmap = clr_to_cmap(cmap, arr)
-
-    elif nb == 3:
-        arr = [read_one_band(ds.GetRasterBand(_+1)) for _ in range(3)]
-        arr = np.dstack(arr)
-        cmap = None
-    else:
-        raise RuntimeError('only 1 or 3 band raster suppoerted')
-
-    info = getinfo(ds)
-
-    plt.figure(figsize=(12,6))
-
-    llcrnrlon=info['Origin'][0]
-    urcrnrlat=info['Origin'][1]
-    urcrnrlon=(info['Origin'][0]+info['Pixel Size'][0] * info['Size'][0])
-    llcrnrlat=(info['Origin'][1]+info['Pixel Size'][1] * info['Size'][1])
-
-    if urcrnrlon > 180:
-        if urcrnrlon - 180 < .1 * info['Pixel Size'][0]:
-            urcrnrlon = 180
-        else:
-            raise RuntimeError('urrcrnerlon: %s' % urcrnrlon)
-    if llcrnrlat < -90:
-        if llcrnrlat - 90 < abs(.1 * info['Pixel Size'][1]):
-            llcrnrlat = -90
-        else:
-            raise RuntimeError('urrcrnerlon: %s' % llcrnrlat)
-    
-    m = Basemap(
-            # need to flip upside down
-            llcrnrlon=llcrnrlon,
-            urcrnrlat=urcrnrlat,
-            urcrnrlon=urcrnrlon, 
-            llcrnrlat=llcrnrlat
-            )
-    m.drawcoastlines()
-    print(arr.shape)
-    m.imshow(arr,
-            cmap=cmap)
-    plt.show()
 
 def band_info( band):
     ds = band.GetDataset()
