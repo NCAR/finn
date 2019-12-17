57caff643c6a54869984d46f8370a3f78d930b39
Yosuke Kimura <yosukefk@users.noreply.github.com>
2019-12-17 14:40:10 -0600

    testpy

--- code_anaconda/step1_prep_v7m.sql
+++ code_anaconda/step1_prep_v7m.sql
@@ -163,6 +163,11 @@ returns text as
 $$
     import sys
     s = str(sys.version)
+    import numpy as np
+    import networkx as nx
+    s = s + ' | numpy: ' + np.__version__
+    s = s + ' | networkx: ' + nx.__version__
+    
     plpy.notice(s)
     s2 = str(sys.path)
     plpy.notice(2)
