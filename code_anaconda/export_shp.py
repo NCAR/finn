7e593e62f71ec78726bfe1975f5900178315aae9
Yosuke Kimura <yosuke@yosukes-air.attlocal.net>
2019-12-22 12:37:33 -0800

    export option to csv_only

--- code_anaconda/export_shp.py
+++ code_anaconda/export_shp.py
@@ -2,7 +2,7 @@ import os, re
 import subprocess
 from subprocess import Popen
 
-def main(odir, schema, tblname, flds, shpname=None):
+def main(odir, schema, tblname, flds, shpname=None, csvonly=False):
     if shpname is None:
         shpname = tblnam + '.shp'
 
@@ -25,15 +25,16 @@ def main(odir, schema, tblname, flds, shpname=None):
 #    p.communicate()
     #subprocess
 
-    ## # also get as shape file, for QA
-    ## ogr2ogr -f "ESRI Shapefile" global_${yr}_div.shp PG:"host=localhost dbname=finn" -sql "select * from global_${yr}.out_div;"
-    #pgsql2shp -f tes -h localhost finn global_${yr}.out_div
-#    cmd = ['pgsql2shp', '-f', tblname, 'finn', '{0}.{1}'.format(schema, tblname)]
-    cmd = ['ogr2ogr', '-progress', '-f', 'ESRI Shapefile', '-overwrite', os.path.join(odir, shpname), 'PG:dbname={0}'.format(os.environ['PGDATABASE']),  
-            '-sql', 'select * from "{schema}"."{tblname}"'.format(schema=schema, tblname=tblname )]
-    print('exporting: %s ...' % os.path.join(odir, shpname), end=' ')
-    subprocess.run(cmd, check=True)
-    print('Done')
-    #p = Popen(cmd)
-    #p.communicate()
-
+    if not csvonly:
+        ## # also get as shape file, for QA
+        ## ogr2ogr -f "ESRI Shapefile" global_${yr}_div.shp PG:"host=localhost dbname=finn" -sql "select * from global_${yr}.out_div;"
+        #pgsql2shp -f tes -h localhost finn global_${yr}.out_div
+    #    cmd = ['pgsql2shp', '-f', tblname, 'finn', '{0}.{1}'.format(schema, tblname)]
+        cmd = ['ogr2ogr', '-progress', '-f', 'ESRI Shapefile', '-overwrite', os.path.join(odir, shpname), 'PG:dbname={0}'.format(os.environ['PGDATABASE']),  
+                '-sql', 'select * from "{schema}"."{tblname}"'.format(schema=schema, tblname=tblname )]
+        print('exporting: %s ...' % os.path.join(odir, shpname), end=' ')
+        subprocess.run(cmd, check=True)
+        print('Done')
+        #p = Popen(cmd)
+        #p.communicate()
+    
