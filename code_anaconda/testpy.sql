38d7009012ce94ead408a3c3b6d706884dae5b8b
Yosuke Kimura <yosukefk@users.noreply.github.com>
2020-06-27 15:49:08 -0500

    more clear test message for plpython

--- code_anaconda/testpy.sql
+++ code_anaconda/testpy.sql
@@ -1,18 +1,40 @@
-create or replace function testpy()
+create extension plpython3u;
+
+create or replace function python_ver()
 returns text as
 $$
     import sys
-    s = str(sys.version)
-    import numpy as np
-    import networkx as nx
-    s = s + ' | numpy: ' + np.__version__
-    s = s + ' | networkx: ' + nx.__version__
+    s = str(sys.version).replace('\n', '')
     
-    #plpy.notice(s)
-    s2 = str(sys.path)
-    #plpy.notice(2)
     return(s)
 $$ 
 language plpython3u volatile;
+select python_ver();
 
-select testpy()
+create or replace function python_path()
+returns setof text as
+$$
+    import sys
+    s = list(sys.path)
+    
+    return(s)
+$$ 
+language plpython3u volatile;
+select python_path();
+
+create or replace function python_pkgs()
+returns setof text as
+$$
+    import numpy as np
+    import scipy as sp
+    import networkx as nx
+    o = []
+    o += [ 'numpy: ' + np.__version__ ]
+    o = o + [ 'scipy: ' + sp.__version__ ] 
+    o = o + [ 'networkx: ' + nx.__version__ ]
+    
+    return(o)
+$$ 
+language plpython3u volatile;
+
+select python_pkgs()
