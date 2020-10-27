026126cac00a276ccd7cf1f6b74e725e6a8c3381
Yosuke Kimura <yosukesabai@hotmail.com>
2020-10-26 22:47:09 -0600

    section 1, 2 and 3

--- work_nrt/dev.py
+++ work_nrt/dev.py
@@ -1,43 +1,210 @@
+# python libraries
 import subprocess
 import os
 import sys
 import psycopg2
+import re
 
-def for_pycharm():
-    os.environ['PGDATABASE'] = 'finn'
-    os.environ['PGPASSWORD'] = 'finn'
-    os.environ['PGUSER'] = 'finn'
+# this should be done somewhere eles?  but before loading tools belose
+# maybe even before this python script got called
+os.environ['PGDATABASE'] = 'finn'
+os.environ['PGPASSWORD'] = 'finn'
+os.environ['PGUSER'] = 'finn'
+os.environ['PGPORT'] = '25432'
+os.environ['PGHOST'] = 'localhost'
+
+# finn preproc codes
+sys.path = sys.path + ['../code_anaconda']
+#import downloader
+import af_import
+#import rst_import
+#import polygon_import
+import run_step1
+import run_step2
+import export_shp
+#import plotter
+import run_extra
+#import notebook_util
 
 
 
-def main(out=None):
-    for_pycharm()
-    if out is None:
-        out = sys.stdout
+def sec1_user_config(tag_af, af_fnames, year_rst):
+
+    # tag to identify datasets, automatically set to be modlct_YYYY, modvcf_YYYY
+    tag_lct = 'modlct_%d' % year_rst
+    tag_vcf = 'modvcf_%d' % year_rst
+
+    # tag for the region number polygon
+    tag_regnum = 'regnum'
+
+    # definition of variables in the raster files
+    rasters = [ 
+        { 
+            'tag': tag_lct, 
+            'kind': 'thematic', 
+            'variable': 'lct' 
+            }, 
+        { 
+            'tag': tag_vcf, 
+            'kind': 'continuous', 
+            'variables': ['tree', 'herb', 'bare'], 
+            }, 
+        { 
+            'tag': tag_regnum, 
+            'kind': 'polygons', 
+            'variable_in': 'region_num', 
+            'variable': 'regnum', 
+            }, 
+        ]
+
+    # save *.shp of the output, so that you can hold onto polygons
+    save_shp_of_output = True
+
+    # save *.html version of this notebook upon exit, so that you can keep records
+    save_html_of_notebook = True
+
+    # deletes entire schema in the database for the AF data processed in this notebook
+    wipe_intermediate_vector_in_db = False
+
+    # deletes hdf files downloaded from EARTHDATA for particular year used in this notebook
+    wipe_downloaded_hdf = False
+
+    # deletes intermediate geotiff files (found in proc_rst_XXX directory) for particular year used in this notebook
+    wipe_intermediate_geotiff = False
+
+    # deletes table of raster data imported into database (praticular year used in this notebook)
+    wipe_intermediate_rst_in_db = False
+
+    return locals()
+
+
+def sec2_check_environment(out):
+
+    #out.write('\n'.join([k + '=' + os.environ[k] for  k in sorted(os.environ)]) + '\n\n')
 
     out.write('system environment inside docker container, for debugging purpose\n\n')
-    out.write('\n'.join([k + '=' + os.environ[k] for  k in sorted(os.environ)]) + '\n\n')
+    subprocess.run(['env' , '|', 'sort'], stdout=out, shell=True)
 
-    conn = psycopg2.connect(dbname = os.environ['PGDATABASE'])
-    cur = conn.cursor()
-    cur.execute('select version();')
-    o = cur.fetchall()
-    out.write('PostgreSQL version : ' + o[0][0] + '\n')
+    subprocess.run(['psql', '-c', 'select version();'], stdout = out)
+    subprocess.run(['psql', '-c', 'select postgis_full_version();'], stdout = out)
 
-    cur.execute('select postgis_full_version();')
-    o = cur.fetchall()
-    out.write('PostGIS version : ' + o[0][0] + '\n')
-    #print(o)
+    subprocess.run(['psql', '-f', '../code_anaconda/testpy.sql'], stdout = out)
+    
 
-    with open('../code_anaconda/testpy.sql') as f:
-        sql = f.read()
-        try:
-            o = cur.execute(sql)
-        except psycopg2.errors.DuplicateObject:
-            pass
-    print(o)
+def sec3_import_af(out):
+    # check input file exists
+    print('checking if input files exist:')
+    re_shp = re.compile('fire_archive_(.*).shp')
+    re_zip = re.compile('DL_FIRE_(.*).shp')
+    re_shp_nrt = re.compile('(MODIS_C6|VNP14IMGTDL_NRT)_(.*).shp')
 
 
+    for i,af_fname in enumerate(af_fnames):
+        print("%s: " % af_fname, end='')
+        
+        pn,fn = os.path.split(af_fname)
+        zname = None
+        
+        if os.path.exists(af_fname):
+            print("exists.")
+            # if .zip file, need to expand.
+            if af_fname[-4:] == '.shp':
+                # you are good
+                print('OK')
+            
+            elif af_fname[-4:] == '.zip':
+                # still need to unzip
+                zname = af_fname
+                m = re_zip.match(af_fname)
+                if m:
+                    arcname = m.group()[0]
+                    sname = 'fire_archive_%s.shp' % arcname
+                else:
+                    # i cannot predict name of shp file...
+                    import zipfile
+                    # find what shp file included...?
+                    raise RuntileError('specify .shp file in af_names list!')
+                    arcname,sname = None, None
+            else:
+                raise RuntimeError('specify .shp file in af_names list!')
+        else:
+            print("doesn't exist.")
+            
+            if af_fname[-4:] == '.shp':
+                # guess the zip file name
+                
+                pn,fn=os.path.split(af_fname)
+                
+                # see if it's the sample giant archive we provide 
+                if fn == 'fire_archive_M6_28864.shp':
+                    zurl = 'https://s3-us-west-2.amazonaws.com/earthlab-finn/2016-global-DL_FIRE_M6_28864.zip'
+                    zn = '2016-global-DL_FIRE_M6_28864.zip'
+                    zname = os.path.join(pn, zn)
+                    sname = fn
+                    if not os.path.exists(zname):
+                        print('downloading the sample AF file: %s' % zn)
+                        subprocess.run(['wget', '-P', pn, zurl], check=True)
+                else:
 
-main()
+                    # see if it's an archive of AF
+                    m = re_shp.match(fn)
+                    if m:
+                        arcname = m.groups()[0]
+                        zname = os.path.join( pn, 'DL_FIRE_%s.zip' % arcname)
+                        sname = fn
+                        print('  found zip: %s' % zname)
+                    else:
+                        # see if it's NRT data
+                        m = re_shp_nrt.match(fn)
+
+                        if m:
+                            # NRT downloads
+                            zname = af_fname[:-4] + '.zip'
+                            sname = fn
+                            print('  found zip: %s' % zname)
+
+
+                        else:
+                            raise RuntimeError('cannot find file: %s' % af_fname)
+            else:
+                raise RuntimeError('cannot find file: %s' % af_fname)
+        if zname:
+            print('unzipping: %s' % zname)
+            subprocess.run(['unzip', '-uo', zname, '-d', os.path.dirname(zname)],
+                          check=True)
+            assert os.path.exists(os.path.join(pn, sname))
+            af_fnames[i] = os.path.join(pn, sname)
+            print('OK: done')
+
+            
+    # TODO this is destructive need to safe guard!
+    # tell user schema is there, list table names and # of row of each.  Ask her to delete manually or something to proceed
+    af_import.main(tag_af, af_fnames)
+
+    print()
+    for i,fn in enumerate(af_fnames):
+        print(fn)
+        tblname = '"af_%s".af_in_%d' % (tag_af, i+1)
+        p = subprocess.run(['psql', '-c', 'select count(*) from %s;' % tblname], stdout=subprocess.PIPE)
+        print(p.stdout.decode())
+
+
+    
+
+
+def main(tag_af='testOTS_092018', af_fnames=[
+        '../sample_datasets/fire/testOTS_092018/fire_archive_M6_23960.shp',
+            '../sample_datasets/fire/testOTS_092018/fire_archive_V1_23961.shp',
+            ], year_rst=2017):
+    out = sys.stdout
+
+    user_config = sec1_user_config(tag_af, af_fnames, year_rst)
+    globals().update(user_config)
+
+    sec2_check_environment(out=out)
+
+    sec3_import_af(out=out)
+
+if __name__ == '__main__':
+    main()
 
