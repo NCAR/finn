9744873863ce3d370f870b860ae02a4aa62ac4af
Yosuke Kimura <yosukefk@users.noreply.github.com>
2020-10-28 00:05:49 -0500

    cleaned a bit

--- work_nrt/work_nrt.py
+++ work_nrt/work_nrt.py
@@ -5,18 +5,9 @@
 ## python libraries
 import os
 import sys
-#import psycopg2
 import re
 import subprocess
 import argparse
-#
-## TODO this should be done somewhere eles?  but before loading tools belose
-## maybe even before this python script got called
-#os.environ['PGDATABASE'] = 'finn'
-#os.environ['PGPASSWORD'] = 'finn'
-#os.environ['PGUSER'] = 'finn'
-#os.environ['PGPORT'] = '25432'
-#os.environ['PGHOST'] = 'localhost'
 #
 ## finn preproc codes
 sys.path = sys.path + ['../code_anaconda']
@@ -31,68 +22,6 @@ import run_extra
 import work_common as common
 
 
-#def sec1_user_config(tag_af, af_fnames, year_rst):
-#
-#    # tag to identify datasets, automatically set to be modlct_YYYY, modvcf_YYYY
-#    tag_lct = 'modlct_%d' % year_rst
-#    tag_vcf = 'modvcf_%d' % year_rst
-#
-#    # tag for the region number polygon
-#    tag_regnum = 'regnum'
-#
-#    # definition of variables in the raster files
-#    rasters = [ 
-#        { 
-#            'tag': tag_lct, 
-#            'kind': 'thematic', 
-#            'variable': 'lct' 
-#            }, 
-#        { 
-#            'tag': tag_vcf, 
-#            'kind': 'continuous', 
-#            'variables': ['tree', 'herb', 'bare'], 
-#            }, 
-#        { 
-#            'tag': tag_regnum, 
-#            'kind': 'polygons', 
-#            'variable_in': 'region_num', 
-#            'variable': 'regnum', 
-#            }, 
-#        ]
-#
-#    # save *.shp of the output, so that you can hold onto polygons
-#    save_shp_of_output = True
-#
-#    # save *.html version of this notebook upon exit, so that you can keep records
-#    save_html_of_notebook = True
-#
-#    # deletes entire schema in the database for the AF data processed in this notebook
-#    wipe_intermediate_vector_in_db = False
-#
-#    # deletes hdf files downloaded from EARTHDATA for particular year used in this notebook
-#    wipe_downloaded_hdf = False
-#
-#    # deletes intermediate geotiff files (found in proc_rst_XXX directory) for particular year used in this notebook
-#    wipe_intermediate_geotiff = False
-#
-#    # deletes table of raster data imported into database (praticular year used in this notebook)
-#    wipe_intermediate_rst_in_db = False
-#
-#    return locals()
-#
-#
-#def sec2_check_environment(out):
-#
-#    #out.write('\n'.join([k + '=' + os.environ[k] for  k in sorted(os.environ)]) + '\n\n')
-#
-#    out.write('system environment inside docker container, for debugging purpose\n\n')
-#    subprocess.run(['env' , '|', 'sort'], stdout=out, shell=True)
-#
-#    subprocess.run(['psql', '-c', 'select version();'], stdout = out)
-#    subprocess.run(['psql', '-c', 'select postgis_full_version();'], stdout = out)
-#
-#    subprocess.run(['psql', '-f', '../code_anaconda/testpy.sql'], stdout = out)
-#    
 
 def sec3_import_af(out):
     # check input file exists
