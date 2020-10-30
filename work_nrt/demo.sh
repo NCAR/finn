b24b871c443597a068f41e55d3df87ec7f0b9991
Yosuke Kimura <yosukesabai@hotmail.com>
2020-10-29 23:21:07 -0600

    viirs input

--- work_nrt/demo.sh
+++ work_nrt/demo.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-python3 ./work_nrt.py -t c6_nrt_2020299 -y 2019 ../data/MODIS_C6_Global_MCD14DL_NRT_2020298.txt ../data/MODIS_C6_Global_MCD14DL_NRT_2020299.txt
+# python3 ./work_nrt.py -t c6_nrt_2020299 -y 2019 \
+# 	../data/MODIS_C6_Global_MCD14DL_NRT_2020298.txt \
+# 	../data/MODIS_C6_Global_MCD14DL_NRT_2020299.txt \
+# 	../data/SUOMI_VIIRS_C2_Global_VNP14IMGTDL_NRT_2020298.txt \
+# 	../data/SUOMI_VIIRS_C2_Global_VNP14IMGTDL_NRT_2020299.txt
+
 #python3 ./work_nrt.py -t c6_nrt_2020299 -y 2019 ../data/MODIS_C6_Global_MCD14DL_NRT_2020299.txt
+
+
+python3 ./work_nrt.py -t modissuomi_nrt_2020299 -y 2019 \
+	../data/MODIS_C6_Global_MCD14DL_NRT_2020298.txt \
+	../data/MODIS_C6_Global_MCD14DL_NRT_2020299.txt \
+	../data/SUOMI_VIIRS_C2_Global_VNP14IMGTDL_NRT_2020298.txt \
+	../data/SUOMI_VIIRS_C2_Global_VNP14IMGTDL_NRT_2020299.txt
