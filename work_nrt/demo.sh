b2e978ca5846b462c896c9c0d53d72b2db20f56d
Yosuke Kimura <yosukesabai@hotmail.com>
2020-11-01 18:21:35 -0700

    cleaned demo*.sh

--- work_nrt/demo.sh
+++ work_nrt/demo.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-# python3 ./work_nrt.py -t c6_nrt_2020299 -y 2019 \
-# 	../data/MODIS_C6_Global_MCD14DL_NRT_2020298.txt \
-# 	../data/MODIS_C6_Global_MCD14DL_NRT_2020299.txt \
-# 	../data/SUOMI_VIIRS_C2_Global_VNP14IMGTDL_NRT_2020298.txt \
-# 	../data/SUOMI_VIIRS_C2_Global_VNP14IMGTDL_NRT_2020299.txt
 
-#python3 ./work_nrt.py -t c6_nrt_2020299 -y 2019 ../data/MODIS_C6_Global_MCD14DL_NRT_2020299.txt
-
-#ddir=../data
-ddir=/home/finn/input_data/fire
+#data_dir=../data
+tag=modvrs_nrt_2020299 
+data_dir=/home/finn/input_data/fire
+out_dir=/home/finn/output_data/fire/$tag
 
 # TODO specify days of interest
+# TODO die prematury, if nothing left after removing by date
+
+# grab annual global raster
+python3 ./work_raster.py -y 2019
 
 
-python3 ./work_nrt.py -t modvrs_nrt_2020299 -y 2019 \
-	$ddir/MODIS_C6_Global_MCD14DL_NRT_2020298.txt \
-	$ddir/MODIS_C6_Global_MCD14DL_NRT_2020299.txt \
-	$ddir/SUOMI_VIIRS_C2_Global_VNP14IMGTDL_NRT_2020298.txt \
-	$ddir/SUOMI_VIIRS_C2_Global_VNP14IMGTDL_NRT_2020299.txt
+# process af
+python3 ./work_nrt.py -t $tag -y 2019 \
+	-o $out_dir \
+       	-fd 2020299 -ld 2020299 \
+	$data_dir/MODIS_C6_Global_MCD14DL_NRT_2020298.txt \
+	$data_dir/MODIS_C6_Global_MCD14DL_NRT_2020299.txt \
+	$data_dir/SUOMI_VIIRS_C2_Global_VNP14IMGTDL_NRT_2020298.txt \
+	$data_dir/SUOMI_VIIRS_C2_Global_VNP14IMGTDL_NRT_2020299.txt
--- work_nrt/demo_oregon.sh
+++ work_nrt/demo_oregon.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+#data_dir=../sample_datasets/fire/testOTS_092018
+tag=testOTS_097017 
+data_dir=/home/finn/input_data/fire/$tag
+out_dir=/home/finn/output_data/fire/$tag
 
-#ddir=../sample_datasets/fire/testOTS_092018
-data_dir=/home/finn/input_data/fire/testOTS_092018
-out_dir=/home/finn/output_data/fire/testOTS_092018
-
+# TODO specify days of interest
+# TODO die prematury, if nothing left after removing by date
 
 # grab  relevant raster
-python3 ./work_raster.py -t testOTS_092018 -y 2017 \
+python3 ./work_raster.py -t $tag -y 2016 \
 	$data_dir/fire_archive_M6_23960.shp \
 	$data_dir/fire_archive_V1_23961.shp
 
 
-python3 ./work_nrt.py -t testOTS_092018 -y 2017 \
+# process af
+python3 ./work_nrt.py -t $tag -y 2016 \
 	-o $out_dir \
 	$data_dir/fire_archive_M6_23960.shp \
 	$data_dir/fire_archive_V1_23961.shp
