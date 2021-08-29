6c7030bf650615eefb08b98586fd5504dce2023d
root <root@ee287c03dc24>
2021-08-29 21:04:01 +0000

    utility code to copy over fireid from one to other

---
+++ work_global_modvrs_2018_firstweek/share_fireid_main_v3.py
@@ -0,0 +1,45 @@
+
+import share_fireid_work_v3 as shf
+import psycopg2
+
+import os
+
+tag0 = 'global_modvrs_2018_firstweek'
+
+#tag = 'global_modvrs_2018_no_lrg_poly_firstweek'
+#tag = 'global_vrs_2018_firstweek'
+tag = 'global_vrs_2018_no_lrg_poly_firstweek'
+year_rst = 2017
+
+schema0 = f'af_{tag0}'
+schema = f'af_{tag}'
+# tag to identify datasets, automatically set to be modlct_YYYY, modvcf_YYYY
+tag_lct = 'modlct_%d' % year_rst
+tag_vcf = 'modvcf_%d' % year_rst
+
+# tag for the region number polygon
+tag_regnum = 'regnum'
+
+# definition of variables in the raster files
+rasters = [
+        {
+            'tag': tag_lct,
+            'kind': 'thematic',
+            'variable': 'lct'
+        },
+        {
+            'tag': tag_vcf,
+            'kind': 'continuous',
+            'variables': ['tree', 'herb', 'bare'],
+        },
+        {
+            'tag': tag_regnum,
+            'kind': 'polygons',
+            'variable_in': 'region_num',
+            'variable': 'regnum',
+        },
+]
+tag_rasters = '_'.join([rst['tag'] for rst in rasters])
+tblname = f"out_{tag_rasters}"
+
+shf.work(schema0, schema, tblname)
