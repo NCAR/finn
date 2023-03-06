39e6cfc65f3f7902cefb178d7e5d6880b16b2f81
Yosuke Kimura <yosukesabai@hotmail.com>
2023-03-06 07:47:58 -0700

    path for af file

--- work_nrt/demo_nrt.sh
+++ work_nrt/demo_nrt.sh
@@ -55,14 +55,14 @@ wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=4 "https://nrt3.modaps.eo
 wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=4 "https://nrt3.modaps.eosdis.nasa.gov/api/v2/content/archives/FIRMS/suomi-npp-viirs-c2/Global/SUOMI_VIIRS_C2_Global_VNP14IMGTDL_NRT_${yjm1}.txt" --header "Authorization: Bearer ${token}" -P $data_dir
 wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=4 "https://nrt3.modaps.eosdis.nasa.gov/api/v2/content/archives/FIRMS/suomi-npp-viirs-c2/Global/SUOMI_VIIRS_C2_Global_VNP14IMGTDL_NRT_${yj}.txt" --header "Authorization: Bearer ${token}" -P $data_dir
 
-# grab annual global raster (can be commented out if you know that it's already imported into the database)
-python3 $exc_dir/work_raster.py -y ${yrm2}
-
-
-if [ $? -ne 0 ]; then
-	echo problem in work_raster.py
-	exit 1
-fi
+### # grab annual global raster (can be commented out if you know that it's already imported into the database)
+### python3 $exc_dir/work_raster.py -y ${yrm2}
+### 
+### 
+### if [ $? -ne 0 ]; then
+### 	echo problem in work_raster.py
+### 	exit 1
+### fi
 
 
 
@@ -71,10 +71,10 @@ python3 $exc_dir/work_nrt.py -t $tag -y $yrm2 \
 	-o $out_dir \
        	-fd $yj -ld $yj \
         -s $summary_file \
-	$data_dir/FIRMS/modis-c6.1/MODIS_C6_Global_MCD14DL_NRT_${yjm1}.txt \
-	$data_dir/FIRMS/modis-c6.1/MODIS_C6_Global_MCD14DL_NRT_${yj}.txt \
-	$data_dir/FIRMS/suomi-npp-viirs-c2/SUOMI_VIIRS_C2_Global_VNP14IMGTDL_NRT_${yjm1}.txt \
-	$data_dir/FIRMS/suomi-npp-viirs-c2/SUOMI_VIIRS_C2_Global_VNP14IMGTDL_NRT_${yj}.txt
+	$data_dir/FIRMS/modis-c6.1/Global/MODIS_C6_Global_MCD14DL_NRT_${yjm1}.txt \
+	$data_dir/FIRMS/modis-c6.1/Global/MODIS_C6_Global_MCD14DL_NRT_${yj}.txt \
+	$data_dir/FIRMS/suomi-npp-viirs-c2/Global/SUOMI_VIIRS_C2_Global_VNP14IMGTDL_NRT_${yjm1}.txt \
+	$data_dir/FIRMS/suomi-npp-viirs-c2/Global/SUOMI_VIIRS_C2_Global_VNP14IMGTDL_NRT_${yj}.txt
 
 if [ $? -ne 0 ]; then
 	echo problem in work_nrt.py
